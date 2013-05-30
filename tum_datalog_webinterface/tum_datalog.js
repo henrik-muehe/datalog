74c3161fa2fc5141fb212ee2181bdefafb07c56d
Harald Lang <harald.lang@gmail.com>
2013-05-30 21:11:33 +0200

    Made the backend configurable and added a computation time limit.

--- tum_datalog_webinterface/tum_datalog.js
+++ tum_datalog_webinterface/tum_datalog.js
@@ -12,23 +12,33 @@ var app = express();
 // serve static content from the 'static' directory
 app.use(express.static('static'));
 
+// serve static content from the 'examples' directory
+app.use("/examples", express.static('examples'));
+
 // use body parser to get access to form data
 app.use(express.bodyParser());
 
-// input validator
+// use the input validator
 app.use(expressValidator);
 
+// add a 'endsWith' function to String
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
 
-var desExecutable = 'H:\\Downloads\\des\\des.exe';
-var port = 3000
+// read configuration file
+var config = JSON.parse(fs.readFileSync('config.json','utf8')); 
 
+// used to create temporary files
+var counter = 0;
 
-// REST service
+// Datalog REST service
 app.post('/datalog', function (req, res) {
 
+	// validate user input
   req.assert('ruleset', 'required').notEmpty();
   req.assert('ruleset', 'max. 4096 characters allowed').len(0, 4096);
-  req.assert('ruleset', 'ruleset contains unallowed characters').is(/^[_=:\-\ \\a-zA-Z0-9\%\n\r(),\.]+$/);
+  req.assert('ruleset', 'ruleset contains unallowed characters').is(/^[_=':\-\+<>\ \\a-zA-Z0-9\%\n\r(),;\.\t]+$/);
 
   req.assert('query', 'required').notEmpty();
   req.assert('query', 'max. 128 characters allowed').len(0, 128);
@@ -42,38 +52,61 @@ app.post('/datalog', function (req, res) {
     return;
   }
 
-  // write the ruleset to a file
-  fs.writeFile('ruleset.dl', req.body.ruleset, function(err) {
+  // write the ruleset to a temporary file
+  counter++;
+  var tempFile = config.tempDirectory + '/ruleset-' + counter + '.dl';
+  fs.writeFile(tempFile, req.body.ruleset, function(err) {
     if(err) {
       console.log(err);
-    } else {
-      console.log("The file was saved!");
-    }
+    } 
   });
 
   // start a DES process and connect the streams
-  var desProcess = spawn(desExecutable);
+  var desProcess = spawn(config.desExecutable);
+  var desProcessIsRunning = true;
+  var computationTimeExceeded = false;
   var answer = '';
   
+  // terminates the process if the computation takes too much time
+  setTimeout(function() {
+  	if (desProcessIsRunning) {
+	  	computationTimeExceeded = true;
+	  	console.log("Terminating DES due to timeout.");
+	  	desProcess.kill('SIGKILL');
+	  	try {
+	  	  fs.unlinkSync(tempFile);
+	    } catch (err) {}
+  	}
+  }, config.desTimeLimitMillis);
+  
+  // reading the standard output of DES
   desProcess.stdout.on('data', function (data) {
     data = '' + data;
     if (!data.match(/^\*/g)) {
       data = data.replace(/DES>/g, '');
       answer = answer + data;
-      console.log(data); 
+      //console.log(data); 
     }
   });
   
+  // send the answer after the computation has been finished
   desProcess.on('exit', function (code) {
-    console.log('child process exited with code ' + code);
-    console.log('done');
+    desProcessIsRunning = false;
+    try {
+    	fs.unlinkSync(tempFile);
+    } catch (err) {}
+    //console.log('child process exited with code ' + code);
+    //console.log('done');
+    if (computationTimeExceeded) {
+    	answer = "---  Sorry, computation time exceeded...  ----"
+    }
     res.json({ 
       'answer': answer
     });
   });
   
   // consult the previously written file
-  desProcess.stdin.write('/consult ' + 'ruleset.dl' + '\n');
+  desProcess.stdin.write('/consult ' + tempFile + '\n');
   
   // submit query
   desProcess.stdin.write(req.body.query + '\n');
@@ -83,6 +116,31 @@ app.post('/datalog', function (req, res) {
   
 });
 
+// Examples REST service. Delivers the examples to the client.
+app.get('/examples', function (req, res) {
 
-app.listen(port);
-console.log('Started web service on port ' + port + '.');
+	// reads the contents of the examples directory
+	// and sends a list to the client.
+	
+	// the examples are delivered as static content
+  var exampleDescriptors = [];
+  var exampleFiles = fs.readdirSync(config.examplesDirectory);
+  exampleFiles.forEach(function(file) {
+    if (file.endsWith(".json")) {
+	  	var fileContents = fs.readFileSync(config.examplesDirectory + '/' + file,'utf8'); 
+			var example = JSON.parse(fileContents);
+			exampleDescriptors.push({
+				"name": example.name,
+				"description": example.description,
+				"source": example.source,
+				"url": "/examples" + "/" + file,
+			});
+		}
+  });
+	res.json(exampleDescriptors);
+  
+});
+
+
+app.listen(config.port);
+console.log('Started web service on port ' + config.port + '.');
